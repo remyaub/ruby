8ff64562079a81aaec792002876b905d5becdbcb
remyaub <45595989+remyaub@users.noreply.github.com>
2019-01-03 10:42:49 +0000

    upload exo 19

---
+++ exo_19.rb
@@ -0,0 +1,26 @@
+puts " Voici 50 emails : "
+
+
+emailsTotal = 50
+start = 0
+
+tab = []
+while start <= emailsTotal
+
+	
+if start < 10
+	email = "jean.dupont0#{start}@email.fr"
+
+else
+	email = "jean.dupont#{start}@email.fr"
+
+	
+end
+
+start += 1
+tab.push(email)
+
+
+end
+
+print tab.values_at(*tab.each_index.select(&:even?))
